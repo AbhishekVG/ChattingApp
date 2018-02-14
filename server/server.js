f5b57d706eb049d78b9e4554cec7c12df0d5c39a
Abhishek B V <VAbhishek@ESTUATE.COM>
2018-02-14 10:27:57 +0530

    added connect and disconnect handlers Instructions were added for express.static and more different type of importing css files from routes through link tag

--- server/server.js
+++ server/server.js
@@ -1,18 +1,35 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const app = express();
-const publicPath = path.join(__dirname, '../public');
+const publicPath = path.join(__dirname, '../public');  //path join resolves the path
 const port = process.env.PORT || 3000;
-
+const server = http.createServer(app);
 app.use(express.static(publicPath));
+const io = socketIO(server)
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+    console.log("New user connected");
+    socket.on('disconnect', () => {
+        console.log("disconnected from the client");
+    })
+})
+
+server.listen(port, () => {
     console.log(`app started listening at the port ${port}`)
 });
 
 
+// app.get('/', (req, res) => {
+//     res.sendFile(publicPath+ '/index.html');  //this will stop rendering of html file showing forbidden, it seems ../ is malicious so better use path.resolve or path.join...this is another way to render html file other than app.use middleware file
+// })
 
+// app.get('/css/styles.css', (req, res) => {
+//     console.log('__dirname/css/style.css', __dirname + '/css/style.css')
+//     res.sendFile(__dirname+ '/css/style.css');
+// }) // saw this working on one of the videos but here it's not working fine //works....give this route in link href tag...
 
 
 
